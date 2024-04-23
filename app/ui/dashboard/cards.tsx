4dade35ec0405fc681580ec7229d837e98c0cbde
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-04-23 11:50:57 +0530

    working on data fetching

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -6,6 +6,8 @@ import {
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
+import { Cards } from '@/app/lib/definitions';
+
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
@@ -18,14 +20,14 @@ export default async function CardWrapper() {
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
